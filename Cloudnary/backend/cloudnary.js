5d89dcc5fd6b875b9d32971f78268cc71cdc1480
ankitatra <ankitathakurankita7007@gmail.com>
2023-08-22 03:24:04 -0700

    add cloudinary frontend backend

--- Cloudnary/backend/cloudnary.js
+++ Cloudnary/backend/cloudnary.js
@@ -1,12 +1,4 @@
-// const cloudinary = require('cloudinary').v2;
 
-// cloudinary.config({ 
-//     cloud_name: "dlrgh9gam", 
-//     api_key:"254299893452357", 
-//     api_secret:"4mZorIis9DEEdRy2MkrqO-6lapo"
-//   });
-
-//   module.exports= cloudinary;
   
 const cloudinary = require('cloudinary');
 
